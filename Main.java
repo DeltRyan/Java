9065ace259bdf7b762da54ffbc316f75b4560852
DeltRyan <ryanf155@nycstudents.net>
2023-09-17 13:11:53 -0400

    main

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
@@ -19,7 +21,42 @@ public class Main {
         System.out.println(weightedAverage);
 
         System.out.print("Did your team win? ");
-        System.out.print(win);
+        System.out.println(win);
+
+        //1.3
+        System.out.println();
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("How old are you? ");
+        int age = myObj.nextInt();
+
+        System.out.println("What year is it now? ");
+        int currentYear = myObj.nextInt();
+
+        int birthYear = currentYear - age;
+        System.out.print("You were born in ");
+        System.out.println(birthYear);
+
+        System.out.println();
+        System.out.println("What is your siblings birth year? ");
+        int yearSibling = myObj.nextInt();
+
+        int ageDifference = age - (currentYear - yearSibling);
+
+        if (ageDifference > 0) {
+            System.out.print("You are ");
+            System.out.print(ageDifference);
+            System.out.println(" years older than your sibling");
+        }
+        if (ageDifference == 0) {
+            System.out.print("You are the same age as your sibling");
+        }
+        if (ageDifference < 0) {
+            System.out.print("You are ");
+            ageDifference = ageDifference * -1;
+            System.out.print(ageDifference);
+            System.out.println(" years younger than your sibling");
+        }
+
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
 
         }
