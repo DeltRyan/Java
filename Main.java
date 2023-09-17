08281da290fabb853df79da1ff56f87acf2238fd
DeltRyan <ryanf155@nycstudents.net>
2023-09-16 23:53:35 -0400

    main

--- Main.java
+++ Main.java
@@ -1,13 +1,26 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
+        //1.2
+        final int numGrapes = 12;
+        double weightedAverage = 97.68;
+        boolean win = false;
+
+        System.out.print("How many grapes are there on the grapevine? ");
+        System.out.println(numGrapes);
+
+        System.out.print("What was your weighted average at the end of the school year? ");
+        System.out.println(weightedAverage);
+
+        System.out.print("Did your team win? ");
+        System.out.print(win);
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
 
         }
     }
-}
