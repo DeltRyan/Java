fc93752ff4b3be0efce9bc1b94aaa92baf2805d9
DeltRyan <ryanf155@nycstudents.net>
2023-09-30 14:48:47 -0400

    main1

--- Age.java
+++ Age.java
@@ -1,30 +1,55 @@
+import java.util.Scanner;
+
 public class Age
 {
-    private boolean older;
     private int currentYear;
     private int yearSibling;
 
+    public void start ()
+    {
+        System.out.println("Hello and welcome!");
+
+        System.out.println();
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("How old are you? ");
+        int age = myObj.nextInt();
+
+        System.out.println("What year is it now? ");
+        int currentYear = myObj.nextInt();
+
+        int birthYear = currentYear - age;
+        System.out.print("You were born in ");
+        System.out.println(birthYear);
+
+        System.out.println();
+        System.out.println("What is your sibling's birth year? ");
+        int yearSibling = myObj.nextInt();
+
+        new Age(currentYear, birthYear, yearSibling);
+    }
+    public Age()
+    {
+        start();
+    }
     public Age(int currentYear, int birthYear, int yearSibling)
     {
         int age = currentYear - birthYear;
         int ageSib = currentYear - yearSibling;
         int ageDifference = age - ageSib;
-        older(ageDifference, age);
         this.currentYear = currentYear;
         this.yearSibling = yearSibling;
+        older(ageDifference, age);
     }
-    public boolean older(int ageDifference, int age)
+    public void older(int ageDifference, int age)
     {
-        System.out.println(this.currentYear);
         double ageDifferenceTimes;
         if (ageDifference > 0)
         {
             System.out.print("You are ");
             System.out.print(ageDifference);
             System.out.println(" years older than your sibling");
-            //older = true;
 
-            ageDifferenceTimes = (double)age / (this.currentYear - this.yearSibling);
+            ageDifferenceTimes = (double)age / (currentYear - yearSibling);
             ageDifferenceTimes = Math.round(ageDifferenceTimes * 100.0) / 100.0;
             System.out.print("You are ");
             System.out.print(ageDifferenceTimes);
@@ -40,16 +65,13 @@ public class Age
             ageDifference = ageDifference * -1;
             System.out.print(ageDifference);
             System.out.println(" years younger than your sibling");
-            //older = false;
 
-            ageDifferenceTimes = (double)(this.currentYear - this.yearSibling) / age;
+            ageDifferenceTimes = (double)(currentYear - yearSibling) / age;
             ageDifferenceTimes = Math.round(ageDifferenceTimes * 100.0) / 100.0;
             System.out.print("You are ");
             System.out.print(ageDifferenceTimes);
             System.out.println(" times younger than your sibling");
         }
-        return older;
     }
-
 }
 
--- Main.java
+++ Main.java
@@ -1,35 +1,8 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-import java.util.Scanner;
 
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        System.out.println();
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("How old are you? ");
-        int age = myObj.nextInt();
-
-        System.out.println("What year is it now? ");
-        int currentYear = myObj.nextInt();
-
-        int birthYear = currentYear - age;
-        System.out.print("You were born in ");
-        System.out.println(birthYear);
-
-        System.out.println();
-        System.out.println("What is your sibling's birth year? ");
-        int yearSibling = myObj.nextInt();
-
-
-        Age test = new Age(currentYear, birthYear, yearSibling);
-
-
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        Age ageCalc = new Age();
     }
 }
