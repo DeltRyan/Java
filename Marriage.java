4d5874cc00f1d3241541169430bdcfe16c85bbfb
DeltRyan <ryanf155@nycstudents.net>
2023-11-05 13:30:48 -0500

    bruh

--- Marriage.java
+++
@@ -1,4 +0,0 @@
-public class Marriage
-{
-    private int yearMarriage;
-}
--- Person.java
+++ Person.java
@@ -5,31 +5,59 @@ public class Person
     private int age;
     private String name;
     private boolean married;
+    private int marriedYear;
+    private String gender;
+    private String namePartner;
 
     Scanner scan = new Scanner(System.in);
+
+    public Person ()
+    {
+        name();
+        age();
+        married();
+    }
+
     public void age()
     {
         System.out.println("How old are you? ");
         this.age = scan.nextInt();
+        System.out.println(age);
+
+        int birthYear = currentYear() - age;
+        System.out.println(birthYear);
     }
 
     public void name ()
     {
         System.out.println("What is your name? ");
         this.name = scan.nextLine();
+        System.out.println(name);
+    }
+    public int currentYear()
+    {
+        System.out.println("What year is it right now?");
+        return scan.nextInt();
+
     }
 
     public void married()
     {
         System.out.println("Are you married? ");
-        String married = scan.nextLine();
+        String married = scan.next();
         married = married.toLowerCase();
-        if (married == "yes")
+        System.out.println(married);
+        if (married.equals("yes"))
         {
             this.married = true;
             System.out.println("Which year did you get married? ");
+            this.marriedYear = scan.nextInt();
+            System.out.println(marriedYear);
+
+            System.out.println("What is your partner's name? ");
+            String partnerName = scan.next();
         }
-        if (married == "no")
+        if (married.equals("no"))
         {
             this.married = false;
         }
