da018dbefae5ce8d9a18db995ff6c2dfcbb3d5a4
DeltRyan <ryanf155@nycstudents.net>
2023-11-19 12:34:16 -0500

    bruh

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Person per = new Person();
+        Person perOne = new Person();
+        Person perTwo = new Person();
+        System.out.println(perOne.equals(perTwo));
     }
 }
--- Person.java
+++ Person.java
@@ -37,6 +37,8 @@ public class Person {
     }
 
     public void married() {
+        String alive = "yes";
+        boolean partnerDead = false;
         System.out.println("Are you married? ");
         String married = scan.next();
         married = married.toLowerCase();
@@ -47,11 +49,27 @@ public class Person {
             this.marriedYear = scan.nextInt();
             System.out.println(marriedYear);
 
-            System.out.println("What is your partner's name? ");
-            String partnerName = scan.next();
+            System.out.println("Is your partner still alive?");
+            alive = scan.next();
+            alive = alive.toLowerCase();
+
         }
-        if (married.equals("no")) {
+        if (married.equals("no"))
+        {
             this.married = false;
         }
+        if(married.equals("yes") && alive.equals("yes"))
+        {
+            System.out.println("What their gender?");
+            this.gender = scan.nextLine();
+        }
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public boolean equals(Person other)
+    {
+        return getName().equals(other.getName());
     }
 }
