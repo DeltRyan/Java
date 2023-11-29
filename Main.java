2393aaf16ebfaab0103de23e6dfa3e42024642db
BT_1N3_25 <ryanf155@nycstudents.net>
2023-11-29 14:02:26 -0500

    for fail

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
+import static java.lang.Integer.valueOf;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many people are in your family? ");
+        String members = scan.nextLine();
+
+        for (int i = 2; i == 0; i--)
+        {
+            Person
+        }
+
         Person perOne = new Person();
         Person perTwo = new Person();
         System.out.println(perOne.equals(perTwo));
